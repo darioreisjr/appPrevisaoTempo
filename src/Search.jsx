bc0753bd2b076245e9a1c7f726ae69b3f2d287f8
Dario Reis <guttyerresjunior1995@gmail.com>
2023-06-30 22:34:44 -0300

    Style:adding styles to the project

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+
 function Search() {
 
   const [cidade, setCidade] = useState("")
@@ -8,24 +9,29 @@ function Search() {
     e.preventDefault();
     setCidade("")
     let currentValue = document.querySelector('input[name=seachInput]').value
-    alert(currentValue)
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${currentValue}&appid=4d8fb5b93d4af21d66a2948710284366&units=metric`;
     fetch(url)
       .then(response => response.json())
       .then(data => {
         const { main, name, sys, weather } = data
+        const tempo = main.temp.toFixed(0);
         if (sys != undefined) {
-
           if (weather != undefined) {
             const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`;
             setCidade(`
-            <p>${main.temp}</p>
-            <p>${sys.country}</p>
-            <p>${name}</p>
-            <p>${weather[0]['description']}</p>
-            <img src="${icon}"/>`
-            )
+            <div class="containerCidade">
+              <div class="containerItem">
+                <img src="${icon}"/>
+                <p>${weather[0]['description']}</p>
+              </div>
+
+              <div class="containerItem">
+                <h1>${tempo} ºC</h1>
+                <p>${name} - ${sys.country}</p>  
+              </div>
+            </div>   
+              `)
           }
         } else {
           setCidade("")
@@ -34,22 +40,20 @@ function Search() {
       })
   }
 
-
-
   return (
     <div className="searchWraper">
       <div className="search" >
         <h2>Digite a cidade que você que saber a previsão... 👇🏼 </h2>
         <form onSubmit={(e) => seachInput(e)}>
           <input placeholder="Digite a cidade..." type="text" name="seachInput" />
-          <input type="submit" value="Pesquisar" />
+          <input type="submit" value="Pesquisar"/>
         </form>
       </div>
 
       {
         (cidade != "") ?
           <div dangerouslySetInnerHTML={{ __html: cidade }} /> :
-          <div>Pesquise por algo acima...</div>
+          <div className="searchPage">Pesquise por algo acima...</div>
       }
 
     </div>
